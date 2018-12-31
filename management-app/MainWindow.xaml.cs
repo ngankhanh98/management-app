ae88f9f574d6a963ecc7e2d0cb8a3c6174ff36e0
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-01-01 00:53:53 +0700

    MainWindown designed

--- management-app/MainWindow.xaml.cs
+++ management-app/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace management_app
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
