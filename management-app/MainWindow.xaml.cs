34685d7810f4880e2f31238e325bcc92d127ac07
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-01-14 09:40:06 +0700

    ADO.NET with entity frameword plugin

--- management-app/MainWindow.xaml.cs
+++ management-app/MainWindow.xaml.cs
@@ -20,9 +20,15 @@ namespace management_app
     /// </summary>
     public partial class MainWindow
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
